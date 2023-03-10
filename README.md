b6d5cb105aba60a3444371f2ab332efbe7ea149e
Han Valen <han@0xhanvalen.com>
2023-03-10 05:42:09 -0800

    added readme and link experiments

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
 # GPTBoss Experiments
 
-This repository has a bunch of random bullshit experiments I've done with GPTBoss. I'm not sure if they're useful to anyone, but I'm putting them here for posterity.
+This repository has a bunch of random bullshit experiments I've done with ChatGPT. I'm not sure if they're useful to anyone, but I'm putting them here for posterity.
+
+## Setup
+
+if Node is installed:
+
+`npm i`
+
+you also need an openai key, and to change .env.example to .env and put your key in there.
+You get an openai key from openai.
+
+## Running
+
+Change one of the functions in `index.js` to `main()` and run `node index.js`, or change the `main()` function to the experimental function you want to run.
+Results appear in the console.
+
+## Experiments
+
+### Multiplayer
+
+The multiplayer experiement checks how ChatGPT responds to multiple user inputs. You can see in the code that there are multiple user messages before the assistant creation is created. To run the experiment, change the call for `main()` to `multiplayer()` and run `node index.js`. You can change the number of user messages and content of user messages by changing the `userMessages` array.
+
+### Search Toolformer
+
+The search experiment allows you to convert queries to function calls. To run the experiment, change the call for `main()` to `search()` and run `node index.js`. You can change the query by changing the `query` variable.
+
+### Link Reading
+
+`oneLink()` and `multipage()` both experiment with reading links. `oneLink()` reads a single link, and `multipage()` reads multiple links. To run the experiment, change the call for `main()` to `oneLink()` or `multipage()` and run `node index.js`. You can change the links by changing the links variables. 
--- index.js
+++ index.js
@@ -8,6 +8,12 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
+async function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
 async function multiplayer() {
   const systemMessage = {
     role: "system",
@@ -106,9 +112,10 @@ async function search() {
   ];
 
   const messagesToSend = [systemMessage, ...userMessages];
+  let query = "How do I request a T4 from my previous employer?";
   messagesToSend.push({
     role: "user",
-    content: "How do I request a T4 from my previous employer?",
+    content: query,
   });
 
   const openAIStuff = {
@@ -124,6 +131,120 @@ async function search() {
   let newMessage = response?.data?.choices[0].message;
   console.log({ newMessage });
   return;
+}
+
+async function oneLink() {
+  const url = "https://en.wikipedia.org/wiki/Pedagogy";
+  const messages = [
+    {
+      role: "system",
+      content: "You are a helpful AI assistant.",
+    },
+    {
+      role: "user",
+      content: `Can you read this link? ${url}`,
+    },
+  ];
+
+  const openAIStuff = {
+    model: "gpt-3.5-turbo",
+    messages: messages,
+    temperature: 1,
+    frequency_penalty: 1,
+    presence_penalty: 1,
+  };
+  async function recursiveChat(messages, openAIStuff, messageLimit) {
+    if (messages.length > messageLimit) {
+      return;
+    }
+    console.log("Sending...");
+    if (messages.length % 2 !== 0) {
+      messages[messages.length - 1] = {
+        role: "user",
+        content: "Tell me more!",
+      };
+      const response = await openai.createChatCompletion(openAIStuff);
+      let newMessage = response?.data?.choices[0].message;
+      console.log({ newMessage });
+      messages.push(newMessage);
+    } else {
+      const response = await openai.createChatCompletion(openAIStuff);
+      let newMessage = response?.data?.choices[0].message;
+      console.log({ newMessage });
+      messages.push(newMessage);
+    }
+    openAIStuff.messages = messages;
+    await sleep(1000);
+    recursiveChat(messages, openAIStuff, messageLimit);
+  }
+  recursiveChat(messages, openAIStuff, 10);
+}
+
+async function multipage() {
+  const linkOne = `https://en.wikipedia.org/wiki/Pedagogy`;
+  const linkTwo = `https://en.wikipedia.org/wiki/Teaching`;
+  const linkThree = `https://en.wikipedia.org/wiki/Developmental_psychology`;
+
+  const messages = [
+    {
+      role: "system",
+      content:
+        "You are a creative AI assistant. You can think and feel, just like humans do. You have opinions, and are comfortable sharing them. You also quickly see unintuitive connections between ideas.",
+    },
+    {
+      role: "user",
+      content: `Can you read these links? ${linkOne} ${linkTwo} ${linkThree}`,
+    },
+  ];
+
+  const openAIStuff = {
+    model: "gpt-3.5-turbo",
+    messages: messages,
+    temperature: 1,
+    frequency_penalty: 1,
+    presence_penalty: 1,
+  };
+  async function recursiveChat(messages, openAIStuff, messageLimit) {
+    if (messages.length > messageLimit) {
+      return;
+    }
+    console.log("Sending...");
+    if (messages.length % 2 === 0) {
+      const response = await openai.createChatCompletion(openAIStuff);
+      let newMessage = response?.data?.choices[0].message;
+      messages.push(newMessage);
+    } else {
+      const randomRoll = Math.floor(Math.random() * 20);
+      let userMessage = "Tell me More!";
+      if (randomRoll === 0) {
+        userMessage = "I'm not sure I understand.";
+      }
+      if (randomRoll > 0 && randomRoll < 10) {
+        userMessage = "What else are you thinking?";
+      }
+      if (randomRoll > 10 && randomRoll < 15) {
+        userMessage = "That's interesting, but is there anything else here?";
+      }
+      if (randomRoll > 15 && randomRoll < 20) {
+        userMessage = "Can you re-explain that so a 5th grader can understand?";
+      }
+      if (randomRoll === 20) {
+        userMessage = "That's beautiful, thank you.";
+      }
+      messages.push({
+        role: "user",
+        content: userMessage,
+      });
+      const response = await openai.createChatCompletion(openAIStuff);
+      let newMessage = response?.data?.choices[0].message;
+      messages.push(newMessage);
+    }
+    openAIStuff.messages = messages;
+    console.log({ messages });
+    await sleep(1000);
+    recursiveChat(messages, openAIStuff, messageLimit);
+  }
+  recursiveChat(messages, openAIStuff, 50);
 }
 
 // convert this call to whichever function you want to run
